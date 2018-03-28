2ba4f7a23237e5b0fc3abbaa0ee0aa43f40829dd
Geovannio Frederico de Jesus Vinhas <geovanniovinhas@live.com>
2018-03-28 11:10:43 -0300

    JavaDoc
    Adicionar javadoc do projeto

---
+++ src/main/java/edu/uepb/web/biblioteca/exception/BibliotecaException.java
@@ -0,0 +1,16 @@
+package edu.uepb.web.biblioteca.exception;
+
+/**
+ * @autor geovanniovinhas <vinhasgeovannio@gmail.com
+ *
+ *
+ */
+public class BibliotecaException extends Exception {
+
+	private static final long serialVersionUID = 5283805519730586043L;
+
+	public BibliotecaException(String message) {
+		super(message);
+	}
+
+}
